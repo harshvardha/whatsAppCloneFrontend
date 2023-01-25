88ce5a900a76d7ff1c5ea721886e0593848444de
harshvardhan singh <harshvardhansingh458@protonmail.com>
2023-01-25 17:31:56 +0530

    to search users

--- src/components/chat/menu/Search.jsx
+++ src/components/chat/menu/Search.jsx
@@ -1,9 +1,50 @@
-import { Box } from "@mui/material"
+import { Box, InputBase, styled } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
-const Search = () => {
+const Wrapper = styled(Box)`
+    background-color: #fff;
+    height: 2.8125rem;
+    border-bottom: 1px solid #f2f2f2;
+    display: flex;
+    align-items: center;
+`;
+
+const Container = styled(Box)`
+    background-color: #f0f2f5;
+    position: relative;
+    margin: 0 0.8125rem;
+    width: 100%;
+    border-radius: 10px;
+`;
+
+const Icon = styled(Box)`
+    position: absolute;
+    height: 100%;
+    padding: 0.375rem 0.5rem;
+    color: #919191;
+`;
+
+const InputField = styled(InputBase)`
+    width: 100%;
+    padding: 1rem;
+    padding-left: 4.0625rem;
+    height: 0.9375rem;
+    font-size: 0.865rem;
+`;
+
+const SearchBox = () => {
     return (
-        <Box>
-
-        </Box>
+        <Wrapper>
+            <Container>
+                <Icon>
+                    <Search fontSize="small" />
+                </Icon>
+                <InputField
+                    placeholder="Search"
+                />
+            </Container>
+        </Wrapper>
     )
 }
+
+export default SearchBox;
