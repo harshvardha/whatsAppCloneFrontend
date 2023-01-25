50704917d508c6fcb523fb63183400ec9c692e04
harshvardhan singh <harshvardhansingh458@protonmail.com>
2023-01-25 17:32:19 +0530

    menu component to navigate to profile

--- src/components/chat/menu/Menu.jsx
+++ src/components/chat/menu/Menu.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import Header from "./Header";
+import SearchBox from "./Search";
+import Conversations from "./Conversations";
 
 export const Menu = () => {
 
     return (
         <Box>
             <Header />
+            <SearchBox />
+            <Conversations />
         </Box>
     )
 }
